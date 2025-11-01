a227ea18ccfe23c930fa1acb03170e541645b7b6
knhu-uel <nhunhk234111e@st.uel.edu.vn>
2025-11-01 08:51:34 +0700

    Delete project_retail directory

--- project_retail/tests/test_LoginMainWindowEx.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from project_retail.ui.LoginMainWindowEx import LoginMainWindowEx
-
-app=QApplication([])
-login_ui=LoginMainWindowEx()
-login_ui.setupUi(QMainWindow())
-login_ui.showWindow()
-app.exec()
